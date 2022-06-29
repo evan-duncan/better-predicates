ced6ffae4c28423080a4c5f8cbe63331c002bcd2
Evan Duncan <evan.e.duncan@gmail.com>
2022-06-28 22:14:56 -0500

    Add number?

--- src/package.lisp
+++ src/package.lisp
@@ -1,4 +1,12 @@
+(defpackage :better-predicates
+  (:use :common-lisp)
+  (:export #:zero?
+           #:number?))
 
 (defmacro zero? (n)
   "Is this number zero?"
   `(zerop ,n))
+
+(defmacro number? (n)
+  "Returns true if n is of type number. Otherwise returns false."
+  `(numberp ,n))
